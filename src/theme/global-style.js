dfe3d823b4c684ba5b234c86aa44d535f4572605
Jakub Ziemba <jayziembka@gmail.com>
2021-03-25 20:16:27 +0100

    Add font-smoothing

--- src/theme/global-style.js
+++ src/theme/global-style.js
@@ -17,6 +17,8 @@ export const GlobalStyle = createGlobalStyle`
 
   *, *:before, *:after {
     box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   }
 
   html {
