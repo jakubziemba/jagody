6a5b71f561730626b574ba5113aa4db6957b1aea
Jakub Ziemba <jayziembka@gmail.com>
2021-03-25 18:55:49 +0100

    Create Footer

---
+++ src/components/page-elements/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function Footer() {
+  return (
+    <FooterWrapper>Copyright &copy;2021. All rights reserved.</FooterWrapper>
+  )
+}
+
+const FooterWrapper = styled.footer`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 3rem;
+  background-color: ${({ theme }) => theme.colors.secondary};
+  color: ${({ theme }) => theme.colors.primary};
+  font-family: ${({ theme }) => theme.fonts.sans};
+  font-size: 0.7rem;
+`
