6f0d97b80ee4f797cf5467aa5c6c3e471391f8cb
Jakub Ziemba <jayziembka@gmail.com>
2021-03-25 20:15:58 +0100

    Add image and styles

--- src/components/page-elements/About.js
+++ src/components/page-elements/About.js
@@ -1,35 +1,66 @@
 import React from 'react'
+import { StaticImage } from 'gatsby-plugin-image'
 import { Wrapper } from '../../styles'
 import styled from 'styled-components'
 
 export default function About() {
   return (
-    <Wrapper>
-      <Text>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam et
-        aperiam, blanditiis quis voluptatum odit officiis quaerat earum ullam
-        assumenda perferendis magnam provident laboriosam est libero expedita
-        voluptas rem modi. Fugit, rerum molestias ad labore modi, quos eveniet
-        doloribus repellendus aliquid sit, non mollitia. Tempore consequatur
-        dicta ducimus laudantium accusamus, reprehenderit beatae necessitatibus
-        incidunt quasi, maxime quis nihil delectus dolores. Lorem ipsum dolor
-        sit amet consectetur adipisicing elit. Dolorem, corrupti omnis
-        voluptatum blanditiis vero facere quisquam similique ut! Blanditiis
-        rerum dicta saepe id, reprehenderit numquam nam sunt minima labore modi.
-        Dolores qui natus explicabo, ipsum ratione sed quidem. Modi, sequi
-        architecto asperiores sunt reiciendis quod doloribus culpa perferendis.
-        Illum vero dolores adipisci asperiores sequi, laborum error tenetur
-        eligendi perspiciatis nesciunt? Quia dolor, facilis sunt pariatur illum
-        quibusdam recusandae error omnis vero quos ipsa, exercitationem
-        incidunt! Magni ipsa, placeat sit sequi eaque aliquam temporibus
-        quibusdam dignissimos libero suscipit maxime accusamus impedit.
-      </Text>
-    </Wrapper>
+    <>
+      <StyledWrapper>
+        <ImageWrapper>
+          <StaticImage
+            src='../../images/me.jpg'
+            quality='100'
+            alt='Marcin Wydra'
+            placeholder='blurred'
+          />
+        </ImageWrapper>
+        <Text>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam et
+          aperiam, blanditiis quis voluptatum odit officiis quaerat earum ullam
+          assumenda perferendis magnam provident laboriosam est libero expedita
+          voluptas rem modi. Fugit, rerum molestias ad labore modi, quos eveniet
+          doloribus repellendus aliquid sit, non mollitia. Tempore consequatur
+          dicta ducimus laudantium accusamus, reprehenderit beatae
+          necessitatibus incidunt quasi, maxime quis nihil delectus dolores.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem,
+          corrupti omnis voluptatum blanditiis vero facere quisquam similique
+          ut! Blanditiis rerum dicta saepe id, reprehenderit numquam nam sunt
+          minima labore modi. Dolores qui natus explicabo, ipsum ratione sed
+          quidem.
+        </Text>
+      </StyledWrapper>
+    </>
   )
 }
+const StyledWrapper = styled(Wrapper)`
+  @media (min-width: 992px) {
+    padding: 5rem 5rem;
+    flex-direction: row;
+  }
+`
 
 const Text = styled.div`
+  font-family: ${({ theme }) => theme.fonts.sans};
   color: ${({ theme }) => theme.colors.primary};
-  font-size: 1.4rem;
-  line-height: 1.3;
+  font-size: 1.3rem;
+  line-height: 1.5;
+
+  @media (min-width: 992px) {
+    margin-left: 2rem;
+    padding: 1rem;
+    font-size: 1.6rem;
+    width: 50%;
+  }
+`
+const ImageWrapper = styled.div`
+  margin: 1rem 0;
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+
+  @media (min-width: 992px) {
+    width: 50%;
+    /* padding: 2rem; */
+  }
 `
