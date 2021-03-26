57958c87d9f3915284dc5445636498c7a627a692
Jakub Ziemba <jayziembka@gmail.com>
2021-03-26 01:20:05 +0100

    Center content for bigger screens

--- src/styles/index.js
+++ src/styles/index.js
@@ -3,8 +3,9 @@ import styled from 'styled-components'
 export const Wrapper = styled.section`
   display: flex;
   flex-direction: column;
+  max-width: 120rem;
   width: 100%;
-  margin: 0;
+  margin: 0 auto;
   padding: 1rem;
 `
 
