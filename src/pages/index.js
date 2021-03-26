178128b7d31497dcddcef84f2d879762aaf26444
Jakub Ziemba <jayziembka@gmail.com>
2021-03-26 16:24:30 +0100

    Move Jumbotron component to separate file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,48 +1,9 @@
 import { graphql, useStaticQuery } from 'gatsby'
 
 import About from 'components/page-elements/About'
-import BackgroundImage from 'gatsby-background-image'
+// import { Jumbotron } from 'styles'
+import Jumbotron from 'components/page-elements/Jumbotron'
 import React from 'react'
-import { motion } from 'framer-motion'
-import styled from 'styled-components'
-
-const transition = { duration: 1.1, ease: [0.6, 0.01, -0.05, 1.05] }
-
-const mainTitle = {
-  initial: { y: 0 },
-  animate: {
-    y: 0,
-    transition: {
-      delayChildren: 0.5,
-      staggerChildren: 0.04,
-      staggerDirection: -1,
-    },
-  },
-}
-
-const subTitle = {
-  initial: { y: 0 },
-  animate: {
-    y: 0,
-    transition: {
-      delay: 0.6,
-      delayChildren: 0.6,
-      staggerChildren: 0.04,
-      staggerDirection: 1,
-    },
-  },
-}
-const letter = {
-  initial: {
-    y: 400,
-  },
-  animate: {
-    y: 0,
-    transition: { ...transition },
-  },
-}
-
-const costam = 'eloelo3210'
 
 export default function Home() {
   const data = useStaticQuery(
@@ -70,88 +31,8 @@ export default function Home() {
 
   return (
     <>
-      <BackgroundImage Tag='section' fluid={imageData}>
-        <Wrapper>
-          <PageHeading>
-            <motion.div style={{ overflow: 'hidden' }}>
-              <motion.h1
-                variants={mainTitle}
-                initial='initial'
-                animate='animate'
-              >
-                {title.split('').map((char, index) => {
-                  return (
-                    <Span key={char + '-' + index} variants={letter}>
-                      {char}
-                    </Span>
-                  )
-                })}
-              </motion.h1>
-            </motion.div>
-            <motion.div style={{ overflow: 'hidden', paddingBottom: '0.2rem' }}>
-              <motion.h3
-                variants={subTitle}
-                initial='initial'
-                animate='animate'
-              >
-                {subtitle.split('').map((char, index) => {
-                  // return space in sentence
-                  if (char === ' ') return ' '
-                  return (
-                    <Span key={char + '-' + index} variants={letter}>
-                      {char}
-                    </Span>
-                  )
-                })}
-              </motion.h3>
-            </motion.div>
-          </PageHeading>
-        </Wrapper>
-      </BackgroundImage>
+      <Jumbotron image={imageData} title={title} subtitle={subtitle} />
       <About />
     </>
   )
 }
-
-const Wrapper = styled.div`
-  height: 100vh;
-`
-
-const PageHeading = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  font-family: ${({ theme }) => theme.fonts.serif};
-  color: ${({ theme }) => theme.colors.secondary};
-  text-transform: uppercase;
-  height: 100%;
-  text-shadow: 0 3px 6px rgba(0, 0, 0, 0.4);
-
-  h1 {
-    margin-bottom: 0.5rem;
-  }
-
-  h3 {
-    font-family: ${({ theme }) => theme.fonts.serif};
-    text-transform: uppercase;
-    font-size: 0.8rem;
-    /* padding-bottom: 0.5rem; */
-  }
-
-  @media (min-width: 992px) {
-    h1 {
-      font-size: 8rem;
-      margin-bottom: 1.4rem;
-    }
-
-    h3 {
-      font-size: 1.5rem;
-    }
-  }
-`
-
-const Span = styled(motion.span)`
-  display: inline-block;
-  position: relative;
-`
--- src/styles/index.js
+++ src/styles/index.js
@@ -1,5 +1,46 @@
+import BackgroundImage from 'gatsby-background-image'
+import React from 'react'
+import { motion } from 'framer-motion'
 import styled from 'styled-components'
 
+// framer-motion settings
+export const transition = { duration: 1.1, ease: [0.6, 0.01, -0.05, 1.05] }
+
+export const mainTitle = {
+  initial: { y: 0 },
+  animate: {
+    y: 0,
+    transition: {
+      delayChildren: 0.5,
+      staggerChildren: 0.04,
+      staggerDirection: -1,
+    },
+  },
+}
+
+export const subTitle = {
+  initial: { y: 0 },
+  animate: {
+    y: 0,
+    transition: {
+      delay: 0.6,
+      delayChildren: 0.6,
+      staggerChildren: 0.04,
+      staggerDirection: 1,
+    },
+  },
+}
+export const letter = {
+  initial: {
+    y: 400,
+  },
+  animate: {
+    y: 0,
+    transition: { ...transition },
+  },
+}
+
+// components
 export const Wrapper = styled.section`
   display: flex;
   flex-direction: column;
