1fac8f6ca8ed1c754cd21003c2c646e86c7d177b
Jakub Ziemba <jayziembka@gmail.com>
2021-03-26 01:21:20 +0100

    Show menu icon on small screens and links on big

--- src/components/page-elements/Header.js
+++ src/components/page-elements/Header.js
@@ -11,6 +11,12 @@ export default function Header() {
       </Logo>
       <Menu>
         <Icon />
+        <Links>
+          <Link to='/plantacja'>Plantacja</Link>
+          <Link to='/galeria'>Galeria</Link>
+          <Link to='/artykuly'>Artykuły</Link>
+          <Link to='/kontakt'>Kontakt</Link>
+        </Links>
       </Menu>
     </Nav>
   )
@@ -27,6 +33,11 @@ const Nav = styled.nav`
   position: fixed;
   top: 0;
   z-index: 10;
+
+  @media (min-width: 992px) {
+    justify-content: space-between;
+    min-height: 4rem;
+  }
 `
 
 const Logo = styled.div`
@@ -39,18 +50,53 @@ const Logo = styled.div`
     color: ${({ theme }) => theme.colors.secondary};
     text-decoration: none;
   }
+
+  @media (min-width: 992px) {
+    padding-left: 5rem;
+    font-size: 2rem;
+  }
 `
 const Menu = styled.div`
   position: absolute;
   right: 0;
   margin: 0;
   margin-right: 8px;
-  width: 24px;
-  height: 24px;
   cursor: pointer;
-  vertical-align: middle;
+
+  @media (min-width: 992px) {
+    /* position: none; */
+    /* display: flex; */
+  }
 `
 const Icon = styled(MenuIconSvg)`
   width: 24px;
   height: 24px;
+
+  @media (min-width: 992px) {
+    display: none;
+    visibility: hidden;
+  }
+`
+
+const Links = styled.div`
+  display: none;
+  visibility: hidden;
+
+  a {
+    font-family: ${({ theme }) => theme.fonts.serif};
+    color: ${({ theme }) => theme.colors.secondary};
+    text-decoration: none;
+    text-transform: uppercase;
+    font-size: 1.05rem;
+    font-weight: bold;
+    margin: 0 1rem;
+    padding: 1rem 1rem;
+  }
+
+  @media (min-width: 992px) {
+    visibility: visible;
+    display: flex;
+    /* justify-content: space-between; */
+    padding-right: 5rem;
+  }
 `
