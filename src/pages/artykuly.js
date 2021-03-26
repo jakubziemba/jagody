70e816fa3763c56e8498889cfc8edf22ec59e5a4
Jakub Ziemba <jayziembka@gmail.com>
2021-03-26 16:06:04 +0100

    Create subpages

---
+++ src/pages/artykuly.js
@@ -0,0 +1,24 @@
+import { graphql, useStaticQuery } from 'gatsby'
+
+import { Jumbotron } from 'styles'
+import React from 'react'
+
+export default function Artykuly() {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        placeholderImage: file(relativePath: { eq: "jumbo.jpeg" }) {
+          childImageSharp {
+            fluid(quality: 100) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    `
+  )
+
+  const imageData = data.placeholderImage.childImageSharp.fluid
+
+  return <Jumbotron image={imageData} title='Artykuły' />
+}
---
+++ src/pages/plantacja.js
@@ -0,0 +1,25 @@
+import { graphql, useStaticQuery } from 'gatsby'
+
+import { Jumbotron } from 'styles'
+import React from 'react'
+import styled from 'styled-components'
+
+export default function Plantacja() {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        placeholderImage: file(relativePath: { eq: "jumbo.jpeg" }) {
+          childImageSharp {
+            fluid(quality: 100) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    `
+  )
+
+  const imageData = data.placeholderImage.childImageSharp.fluid
+
+  return <Jumbotron image={imageData} title='Plantacja' />
+}
